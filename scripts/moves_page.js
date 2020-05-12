7bda5405b6c497a62dee50377ac63069ca040915
Dylan Craine <dylan.craine@deepwatercreations.com>
2020-05-12 02:05:30 -0600

    Add color panel page

---
+++ scripts/moves_page.js
@@ -0,0 +1,5 @@
+var maximize  = function(panel_page){
+    location.href = panel_page;
+    // document.getElementById("main_moves_container").innerHTML=`<object type="text/html" data="${panel_page}"></object>`;
+    // history.pushState({}, "", "color_panel.html");
+}
