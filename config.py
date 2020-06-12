98dd6f14b818435b38948ab5b46d61c6b4bbc45d
Dylan Craine <dylan.craine@deepwatercreations.com>
2020-06-11 23:57:16 -0600

    Add SQLAlchemy

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'thisisatestkey'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False #But don't I want this so I can update live when people add posts?
+                                            #A refresh button wouldn't be the end of the world, but...
+
+
