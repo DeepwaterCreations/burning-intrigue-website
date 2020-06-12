d104259d074d4e3a34fe0f91c2bb454b1417580a
Dylan Craine <dylan.craine@deepwatercreations.com>
2020-06-12 00:42:51 -0600

    Add posts table

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 class User(db.Model):
@@ -6,5 +7,19 @@ class User(db.Model):
     email = db.Column(db.String(120), index = True, unique = True)
     password_hash = db.Column(db.String(128))
 
+    posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class Post(db.Model):
+    #Can I subclass this for Color, Action, Maneuver, Comms?
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String(280))    #Subclasses should replace 'body', obvs
+    timestamp = db.Column(db.DateTime, index = True, default = datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+
