ef0c01ee6e46b41e6d925644f3b19924b0eab651
Dylan Craine <dylan.craine@deepwatercreations.com>
2020-07-05 21:59:54 -0600

    Remove scripts folder
    Obviously, I'm using Python now, not Javascript.

--- scripts/moves_page.js
+++
@@ -1,5 +0,0 @@
-var maximize  = function(panel_page){
-    location.href = panel_page;
-    // document.getElementById("main_moves_container").innerHTML=`<object type="text/html" data="${panel_page}"></object>`;
-    // history.pushState({}, "", "color_panel.html");
-}
